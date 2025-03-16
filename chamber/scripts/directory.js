c6fd39952030dafe7bb00dfc37bd685f97ba86b5
mkanderson2024 <kalani2014.ma@gmail.com>
2025-03-16 14:38:43 -0600

    saving work

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -19,6 +19,83 @@ hambutton.addEventListener('click', () => {
 });
 
 //-------Constants--------
-const url = "https://github.com/mkanderson2024/wdd231/blob/main/chamber/json/chamber.json";
+const url = "data/members.json";
 const cards = document.querySelector('#business-cards');
+
+//Display Buttons
+const gridbutton = document.querySelector("#grid");
+const listbutton = document.querySelector("#list");
+const display = document.querySelector("article");
+
+gridbutton.addEventListener("click", () => {
+    // example using arrow function
+    display.classList.add("grid");
+    display.classList.remove("list");
+});
+
+listbutton.addEventListener("click", showList); // example using defined function
+
+function showList() {
+    display.classList.add("list");
+    display.classList.remove("grid");
+}
+
+//-------Functions-------
+
+getBusinessData();
+//Initial Function
+async function getBusinessData() {
+    try {
+        const response = await fetch("data/members.json");
+        const data = await response.json();
+        console.table(data.members);
+        displayMembers(data.members)
+    } catch (error) {
+        console.error("Error getting member data:", error);
+    }
+
+}
+
+const displayMembers = (members) => {
+    members.forEach((member) => {
+        let card = document.createElement("section");
+        let name = document.createElement("h4");
+        let owner = document.createElement("h5");
+        let phoneNumber = document.createElement("h5");
+        let website = document.createElement("h5");
+        let image = document.createElement("img");
+        let memberLevel = document.createElement("h5");
+        let status = "";
+        // Sets member status
+        if (member.memberLevel == "3")
+            status = "Gold";
+        else if (member.memberLevel == "2")
+            status = "Silver";
+        else
+            status = "Member"
+
+        name.textContent = `${member.name}`;
+
+        image.setAttribute('src', member.imageUrl);
+        image.setAttribute('alt', `Image of ${name}`);
+        image.setAttribute('loading', 'lazy');
+        image.setAttribute('width', '340');
+        image.setAttribute('height', '440');
+
+        owner.textContent = `${member.owner}`;
+        phoneNumber.textContent = `${member.phoneNumber}`
+        website.textContent = `${member.website}`
+        memberLevel.textContent = `Member status: ${status}`
+
+        card.appendChild(name);
+        card.appendChild(image);
+        card.appendChild(phoneNumber);
+        card.appendChild(website);
+        card.appendChild(owner);
+        card.appendChild(memberLevel);
+
+        cards.appendChild(card)
+
+    });
+}
 
