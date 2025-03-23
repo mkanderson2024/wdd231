3e4cfdf818acdce96fb37a970a766a1a7320c2cd
mkanderson2024 <kalani2014.ma@gmail.com>
2025-03-23 16:34:18 -0600

    saving work

---
+++ chamber/scripts/forecast.js
@@ -0,0 +1,22 @@
+const apiKey = "d862572dba4bd221df68a15e28f380a7";
+const city = "Brigham City";
+const url = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+
+fetch(url)
+    .then(response => response.json())
+    .then(forecastData => {
+        // Process the forecast data
+
+        console.log(forecastData);
+    })
+    .catch(error => {
+        console.error("Error fetching data:", error);
+    });
+
+const forecastList = data.list;
+forecastList.forEach(forecast => {
+    const dateTime = new Date(forecast.dt * 1000); // Convert timestamp to date
+    const temperature = forecast.main.temp;
+    const description = forecast.weather[0].description;
+    console.log(`${dateTime.toLocaleString()}, Temp: ${temperature}°C, ${description}`);
+});
