c4bda232be429427d39ad0277155226ba2ecd437
mkanderson2024 <kalani2014.ma@gmail.com>
2025-04-05 15:11:39 -0600

    saving work

---
+++ chamber/scripts/explore.js
@@ -0,0 +1,28 @@
+import { places } from '../data/places.mjs'
+console.log(places)
+
+const allCards = document.querySelector("#cards")
+
+function displayItems(places) {
+    places.forEach(place => {
+        const placeCard = document.createElement('div')
+
+        const placePhoto = document.createElement('img')
+        placePhoto.src = `${place.photo_url}`
+        placePhoto.alt = place.name
+        console.log(placePhoto)
+        placeCard.appendChild(placePhoto)
+
+        const placeTitle = document.createElement('h2')
+        placeTitle.innerText = place.name
+        placeCard.appendChild(placeTitle)
+
+        const placeAddress = document.createElement('address')
+        placeAddress.innerText = place.description
+        placeCard.appendChild(placeAddress)
+
+        allCards.appendChild(placeCard)
+    })
+}
+
+displayItems(places)
